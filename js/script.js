d49dfec614219fbcfd3efd49b364743f6fc26ae8
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-04-04 16:01:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 // Data
 const clicks = document.getElementById('loc');
 const input = document.getElementById('cImg');
-const wtt = document.getElementById('wtt');
 
 var totalClicks = 0;
 var wToType = "var loc = 0";
 
-// Event listeners
-input.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      if(input.value = wToType) {
-        totalClicks += 1
-        clicks.textContent = "Lines of code:" + totalClicks;                                                                                                                                                                                    
-      }
-    }
-});
+// functions
+function checkCode() {
+  if(input.innerHTML = wToType) {    
+    totalClicks += 1
+    clicks.textContent = "Lines of code:" + totalClicks;                                                                                                                                                                                    
+  }
+};
 
