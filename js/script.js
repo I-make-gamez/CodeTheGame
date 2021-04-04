e7717d0a7db0f9cfd594c82dc7c14272b1c51ab9
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-04-04 15:52:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,20 +4,15 @@ const input = document.getElementById('cImg');
 const wtt = document.getElementById('wtt');
 
 var totalClicks = 0;
-var wToType = "var loc = 0;";
-
-// functions
-function checkCode() {
-  if(input.innerHTML === wToType) {
-    totalClicks += 1
-    clicks.textContent = "Lines of code:" + totalClicks;                                                                                                                                                                                    
-  }
-}
+var wToType = "var loc = 0";
 
 // Event listeners
 input.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-      checkCode();
+      if(input.value = wToType) {
+        totalClicks += 1
+        clicks.textContent = "Lines of code:" + totalClicks;                                                                                                                                                                                    
+      }
     }
 });
 
