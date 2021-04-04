a5bea9205f7ed1de5edb217ccf28d8bfa8f3323d
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-04-04 16:35:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Data
 const clicks = document.getElementById('loc');
-const input = document.getElementById('input').value;
+const input = document.getElementById('input');
 const check = document.getElementById('check');
 
 var totalClicks = 0;
@@ -8,7 +8,7 @@ var wToType = "var loc = 0";
 
 // functions
 function checkCode() {
-  if(input === wToType) {    
+  if(input.value === wToType) {    
     totalClicks += 1
     clicks.textContent = "Lines of code:" + totalClicks;                                                                                                                                                                                    
   }
