adff344bf87d638d591ea7f36b3e4da039432b39
I-make-gamez <ebelcher143@gmail.com>
2021-10-29 17:39:50 +0000

    massive update

--- js/script.js
+++ js/script.js
@@ -2,18 +2,48 @@
 const clicks = document.getElementById('loc');
 const input = document.getElementById('input');
 const check = document.getElementById('check');
+const up = document.getElementById('wtt');
 
-var totalClicks = 0;
-var wToType = "var loc = 0";
-
+let wToType = `var loc = 0`
+var totalClicks = 0
 // functions
 function checkCode() {
   if(input.value === wToType) {    
     totalClicks += 1
+    let wToType = `var loc = ${totalClicks}`
     clicks.textContent = "Lines of code:" + totalClicks;
-    input.style.content = " ";
+    bou();
+    clearInput();
   }
+};
+  
+function clearInput(){
+  input.value = null
+};
+
+function keyCode(Event){
+  if(Event = 13){
+    checkCode();
+    return;
+  }
+};
+
+
+//!!THIS FUNCTION CONTROLS EVERY UPGRADE!!
+function bou(){
+  if(totalClicks <= 5){
+    up.innerHTML = `Type: "var loc += 1"`
+    wToType = `var loc += 1`
+  }else if(totalClicks = 5){
+    up.innerHTML = 'Great! Now for some upgrades! first, type {div} {/div}'
+    wToType = `{div} {/div}`
+  }else if(totalClicks = 6){
+    up.innerHTML = `Now, lets fill in our div element. first, add {h1}Loc multiplier{/h1}`
+    wToType = `{h1}Loc multiplier{/h1}`
+  }
+  
 };
 
 // Event listeners
 check.addEventListener("click", checkCode, false);
+document.addEventListener("keydown", keyCode, false)
